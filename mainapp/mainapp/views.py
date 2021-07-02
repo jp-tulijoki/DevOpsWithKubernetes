c39fa4a8e31a8b3180f12df50eaade081c1ee0b2
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2021-07-02 08:56:25 +0300

    exercise 1.11

--- mainapp/mainapp/views.py
+++ mainapp/mainapp/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
 from mainapp.generator import StringGenerator
 import sys
+import os.path
 
 generator = StringGenerator()
 if not "migrate" in sys.argv:
     generator.start()
 
+directory = "./files/"
+filename = "pongs.txt"
+file_path = os.path.join(directory, filename)
+
 def returnStringAndTimestamp(request):
-    return HttpResponse(f"{generator.timestamp} {generator.random_string}", content_type="text/plain")
+    try:
+        with open(file_path) as file:
+            pongs = file.read()
+            return HttpResponse(f"{generator.timestamp} {generator.random_string}\n{pongs}", content_type="text/plain")
+    except:
+        print("file not found")
