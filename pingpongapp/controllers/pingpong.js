4306bc6ddd51de4ef7c99888a08eb665721eb8b5
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2021-07-05 23:28:49 +0300

    exercise 1.11

--- pingpongapp/controllers/pingpong.js
+++ pingpongapp/controllers/pingpong.js
@@ -1,9 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
 const pingpongRouter = require("express").Router();
+const directory = __dirname + "/../files";
+if (!fs.existsSync(directory)) {
+  fs.mkdirSync(directory);
+}
+
+const filePath = path.join(directory, "pongs.txt");
+
 let pong = 0;
 
 pingpongRouter.get("/", (request, response) => {
   pong += 1;
-  return response.json({ pong: `${pong - 1}` });
+  fs.writeFile(filePath, `Ping / Pongs: ${pong}`, (err) => {
+    if (err) return console.log(err);
+  });
+  return response.json({ pong: `${pong}` });
 });
 
 module.exports = pingpongRouter;
